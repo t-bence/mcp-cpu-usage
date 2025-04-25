988982f77c57d199c55eef5cad2958768dcf61e1
t-bence <toth.bence.mihaly@gmail.com>
2025-04-25 10:43:22 +0200

    Refactor

--- cpu_usage/server.py
+++ cpu_usage/server.py
@@ -1,3 +1,5 @@
+import time
+
 import psutil
 from mcp.server.fastmcp import FastMCP
 
@@ -10,17 +12,28 @@ def get_secret_number() -> int:
     return 13
 
 
+from dataclasses import dataclass
+
+
+@dataclass
+class ProcessInfo:
+    name: str
+    percent: float
+
+
 @mcp.tool()
-def list_processes_cpu_usage() -> dict:
+def list_processes_cpu_usage() -> list[ProcessInfo]:
     """Lists processes and their CPU usage."""
-    processes = {}
-    for proc in psutil.process_iter(["pid", "name", "cpu_percent"]):
+    processes: list[ProcessInfo] = []
+    # You need to call the process listing TWICE! The first will return 0.0
+    # for all CPU times, only the second will retrieve true values.
+    # You also need to wait at least 0.1 s between the two calls.
+    psutil.process_iter(["pid", "name", "cpu_percent", "cpu_times"])
+    time.sleep(0.2)
+    for proc in psutil.process_iter(["pid", "name", "cpu_percent", "cpu_times"]):
         try:
-            info = proc.info
-            processes[info["pid"]] = {
-                "name": info["name"],
-                "cpu_percent": info["cpu_percent"],
-            }
+            processes.append(ProcessInfo(proc.name(), proc.cpu_percent()))
+
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             continue
     return processes
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from cpu_usage import server
 
 if __name__ == "__main__":
-    print(server.list_processes_cpu_usage())
+    for proc in server.list_processes_cpu_usage():
+        if proc.percent:
+            print(f"{proc.name}:{proc.percent:>8}")
